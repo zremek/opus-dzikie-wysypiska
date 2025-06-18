713538c0559c6836055a36ef80004525c8704e58
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2025-06-18 12:38:29 +0200

    pkgs to use

---
+++ pkgs.R
@@ -0,0 +1,9 @@
+# for onedrvive / sharepoint to link to working files 
+# https://cran.r-project.org/web/packages/Microsoft365R/vignettes/od_sp.html 
+
+library(Microsoft365R)
+
+# for timeline viz 
+# https://github.com/daattali/timevis?tab=readme-ov-file 
+
+library(timevis)
