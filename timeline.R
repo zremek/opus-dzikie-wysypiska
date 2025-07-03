fbfc3c5e5f748cbe989a50afb4e11b982aa2948f
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2025-07-03 11:49:57 +0200

    vis for paper done

--- timeline.R
+++ timeline.R
@@ -37,29 +37,37 @@ library(vistime)
 
 vistime(d, col.event = "content", col.start = "start", col.group = NA)
 
-gg_vistime(d, )
-
-# ggplot + ggrepel
-
-library(ggrepel)
-
-plants.tb <-
-  data.frame(what = c("sowing", "first emergence", "last emergence", "Dualex",
-                      "treatment start", "Dualex", "harvest"),
-             when = ymd(c("2020-05-01", "2020-05-06", "2020-05-11", "2020-06-21",
-                          "2020-06-22", "2020-06-29", "2020-06-30")),
-             series = "Experiment 1")
 
 
-ggplot(plants.tb, aes(x = when, y = series, label = what, colour = what)) +
-  geom_line() +
-  geom_point() +
-  geom_text_repel(direction = "y",
+
+dg <- d
+dg$series <- "TOYA"
+
+dg <- dg %>% mutate(
+  g = forcats::fct_other(group, keep = c("Nasze Sprawy", "Wydarzenia"), other_level = "Inne")
+)
+
+# for paper #### 
+
+Sys.setlocale("LC_TIME", "pl_PL.UTF-8")
+
+ggplot(data = dg, aes(x = start, y = series, label = id, colour = g)) +
+  geom_line(colour = "gray") +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_label_repel(direction = "y",
                   point.padding = 0.5,
-                  hjust = 0,
-                  box.padding = 1,
-                  seed = 123) +
-  scale_x_date(name = "", date_breaks = "1 months", date_labels = "%d %B",
-               expand = expansion(mult = c(0.12, 0.12))) +
+                  hjust = 0.5,
+                  box.padding = 0.1,
+                  max.overlaps = 100,
+                  min.segment.length = 0.1,
+                  force = 10) +
+  scale_x_date(name = "", date_breaks = "1 year", date_labels = "%b %Y", date_minor_breaks = "1 month"
+               ) +
   scale_y_discrete(name = "") +
-  theme_minimal()
+  scale_color_discrete(name = "Program:", ) +
+  theme_minimal() +
+  theme(legend.position = "bottom") 
+
+ggsave("timeline-toya-te-butelki.png", width = 250, height = 150, units = "mm")
+
+
